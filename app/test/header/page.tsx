c3d8559ab0f9748afaad9d8dd929baa58d228b1b
k0nghaa <k0nghaalee@gmail.com>
2025-12-28 16:19:16 +0900

    fix: 빌드 오류로 header 테스트 페이지 수정

--- app/test/header/page.tsx
+++ app/test/header/page.tsx
@@ -1,5 +1,11 @@
+// test/header/page.tsx
+"use client";
+
+import { useState } from "react";
 import SideHeader from "@/app/components/Header/SideHeader";
 
 export default function HeaderPage() {
-  return <SideHeader />;
+  const [isOpen, setIsOpen] = useState(true);
+
+  return <SideHeader isOpen={isOpen} onClick={() => setIsOpen(false)} />;
 }
