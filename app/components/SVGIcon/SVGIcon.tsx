13b267767ad976a1897084a405554bf44c01ead6
DESKTOP-9RTNCQK\User <leedongguk0@gmail.com>
2025-12-29 11:36:43 +0900

    fix: svgicon react.fc -> forwardRef로 변경

--- app/components/SVGIcon/SVGIcon.tsx
+++ app/components/SVGIcon/SVGIcon.tsx
@@ -1,33 +1,31 @@
-import React from "react";
-
+import { forwardRef, ComponentPropsWithoutRef } from "react";
 import { IconMap, IconMapTypes, IconSizeTypes, IconSizes } from "./iconMap";
 
-export interface SVGIconProps extends Omit<
-  React.SVGProps<SVGSVGElement>,
-  "width" | "height" | "viewBox"
-> {
+type SVGIconProps = Omit<
+  ComponentPropsWithoutRef<"svg">,
+  "width" | "height" | "viewBox" | "children"
+> & {
   icon: IconMapTypes;
   size?: IconSizeTypes | number;
   width?: number;
   height?: number;
-}
-
-const SVGIcon: React.FC<SVGIconProps> = ({
-  icon,
-  size = "md",
-  width,
-  height,
-  ...props
-}) => {
-  const Icon = IconMap[icon];
-
-  if (!Icon) return null;
-
-  const dimension = typeof size === "number" ? size : IconSizes[size];
-
-  return (
-    <Icon {...props} width={width || dimension} height={height || dimension} />
-  );
 };
+
+const SVGIcon = forwardRef<SVGSVGElement, SVGIconProps>(function SVGIcon(
+  { icon, size = "md", width, height, ...props },
+  ref
+) {
+  const Icon = IconMap[icon];
+  if (!Icon) return null;
+  const dimension = typeof size === "number" ? size : IconSizes[size];
+  return (
+    <Icon
+      ref={ref}
+      width={width ?? dimension}
+      height={height ?? dimension}
+      {...props}
+    />
+  );
+});
 
 export default SVGIcon;
