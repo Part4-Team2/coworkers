c734140f8ec292174db4ae1b94715523530db71b
kminsoo0110 <kminsoo0110@naver.com>
2025-12-30 15:17:45 +0900

    feat: add bestarticle component

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -3,18 +3,24 @@
 import clsx from "clsx";
 import { ChangeEvent, useState } from "react";
 import BoardInput from "./components/BoardInput";
+import BestArticle from "./components/BestArticle";
 
 function BoardPage() {
   const [inputVal, setInputVal] = useState("");
 
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div
+      className={clsx(
+        "min-h-screen flex flex-col gap-20 justify-center items-center"
+      )}
+    >
       <BoardInput
         value={inputVal}
         onChange={(e: ChangeEvent<HTMLInputElement>) =>
           setInputVal(e.target.value)
         }
       />
+      <BestArticle />
     </div>
   );
 }
