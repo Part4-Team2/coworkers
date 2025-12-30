dcdba31b95745b66166b86e68003f8f9ab891831
kminsoo0110 <kminsoo0110@naver.com>
2025-12-30 23:48:35 +0900

    fix: make boardinput do flexible shrink

--- app/board/components/BoardInput.tsx
+++ app/board/components/BoardInput.tsx
@@ -9,28 +9,31 @@ interface BoardInputProps {
 
 // 자유게시판에 쓰이는 검색바 입니다.
 // 임시로 최대 수용 글자수를 50으로 설정했습니다.
+// 추후에 어떤 방식으로 작동할지는 모르겠습니다.
 function BoardInput({ value, onChange }: BoardInputProps) {
   return (
     <div
       className={clsx(
-        "w-343 sm:w-696 lg:w-1200 bg-background-secondary p-16",
-        "flex gap-12",
+        "w-full max-w-1200 bg-background-secondary mx-auto p-16",
         "rounded-xl border border-text-primary/10",
         "text-text-default text-base",
         "focus-within:border-text-primary"
       )}
     >
-      <div>
-        <SVGIcon icon="comment" />
+      <div className="flex gap-12">
+        {/* 아이콘은 추후에 돋보기 모양으로 변경 예정 */}
+        <div>
+          <SVGIcon icon="comment" />
+        </div>
+        <input
+          type="text"
+          className="flex-1 bg-transparent outline-0"
+          placeholder="검색어를 입력해주세요"
+          value={value}
+          onChange={onChange}
+          maxLength={50}
+        />
       </div>
-      <input
-        type="text"
-        className="flex-1 bg-transparent outline-0"
-        placeholder="검색어를 입력해주세요"
-        value={value}
-        onChange={onChange}
-        maxLength={50}
-      />
     </div>
   );
 }
