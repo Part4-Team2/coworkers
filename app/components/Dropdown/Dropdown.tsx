10fab399777988a146aa5716060ac3c73cdf514a
kminsoo0110 <kminsoo0110@naver.com>
2026-01-01 23:37:59 +0900

    fix: add only trigger, list options

--- app/components/Dropdown/Dropdown.tsx
+++ app/components/Dropdown/Dropdown.tsx
@@ -5,22 +5,27 @@ import SVGIcon from "../SVGIcon/SVGIcon";
 import { IconMapTypes } from "../SVGIcon/iconMap";
 import { useState, useEffect, useRef } from "react";
 
-type DropdownSize = "md" | "sm";
+export type DropdownSize = "md" | "sm";
+export type TriggerStyle = "text" | "icon";
 
-// 드롭다운 활용시 문자열 배열을 집어넣어주시면 됩니다.
+// trigger로 dropdown style 지정 가능합니다.
+// icon으로 원하는 아이콘으로 적용할 수 있습니다.
 interface DropdownProps {
   options: string[];
   onSelect: (value: string) => void;
   size: DropdownSize;
+  trigger: TriggerStyle;
   value: string;
   icon?: IconMapTypes;
 }
 
+// Text로 정할시 반영되는 크기. Default "md"
 const sizeClass: Record<DropdownSize, string> = {
   sm: "w-94 text-sm p-8",
   md: "w-120 text-md px-14 py-10",
 };
 
+// Text/Icon 공통으로 반영되는 List 크기. Default "md"
 const sizeListClass: Record<DropdownSize, string> = {
   sm: "w-94 text-sm",
   md: "w-120 text-md",
@@ -30,22 +35,21 @@ function Dropdown({
   options,
   onSelect,
   size = "md",
+  trigger,
   value,
   icon = "toggle",
 }: DropdownProps) {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement | null>(null);
 
-  const toggleDropdown = (): void => {
-    setIsOpen((isOpen) => !isOpen);
-  };
+  const toggleDropdown = () => setIsOpen((prev) => !prev);
 
-  const handleSelect = (option: string): void => {
+  const handleSelect = (option: string) => {
     onSelect(option);
     setIsOpen(false);
   };
 
-  const handleClickOutside = (e: MouseEvent): void => {
+  const handleClickOutside = (e: MouseEvent) => {
     if (
       dropdownRef.current &&
       !dropdownRef.current.contains(e.target as Node)
@@ -54,61 +58,74 @@ function Dropdown({
     }
   };
 
-  // 바깥 클릭을 하면 사라집니다.
   useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-
+    document.addEventListener("click", handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     };
   }, []);
 
-  return (
-    <div
-      ref={dropdownRef}
-      className={clsx(
-        `${sizeClass[size]}`,
-        "rounded-xl bg-background-secondary relative"
-      )}
-    >
-      <div className="flex justify-between">
-        <span className="overflow-hidden text-ellipsis line-clamp-1">
-          {value}
-        </span>
-        {/* 다른 아이콘이나 버튼을 받을 수 있게 해야댐. */}
-        <span className="cursor-pointer" onClick={toggleDropdown}>
-          <SVGIcon icon={icon} size="md" />
-        </span>
-      </div>
-      {/* popover 형태로 짜야한다. */}
-      {isOpen && (
-        <ul
+  // 트리거에 따라 나오는 UI가 다릅니다.
+  const renderTrigger = () => {
+    if (trigger === "text") {
+      return (
+        <div
           className={clsx(
-            `${sizeListClass[size]}`,
-            "bg-background-secondary absolute top-full right-0 z-50",
-            "border border-border-primary rounded-xl overflow-hidden"
+            sizeClass[size],
+            "flex justify-between rounded-xl bg-background-secondary cursor-pointer"
           )}
+          onClick={toggleDropdown}
         >
-          {options.map((option) => {
-            const isSelected = value === option;
+          <span className="overflow-hidden text-ellipsis line-clamp-1">
+            {value}
+          </span>
+          <SVGIcon icon={icon} size="md" />
+        </div>
+      );
+    }
 
-            return (
-              <li
-                key={option}
-                onClick={() => {
-                  handleSelect(option);
-                }}
-                className={clsx(
-                  `${isSelected ? "bg-background-tertiary" : "bg-background-secondary hover:bg-background-primary"}`,
-                  "cursor-pointer p-8"
-                )}
-              >
-                {option}
-              </li>
-            );
-          })}
-        </ul>
-      )}
+    return (
+      <span onClick={toggleDropdown} className="cursor-pointer">
+        <SVGIcon icon={icon} size="md" />
+      </span>
+    );
+  };
+
+  // 트리거를 누르면 리스트가 등장합니다.
+  const renderList = () =>
+    isOpen && (
+      <ul
+        className={clsx(
+          sizeListClass[size],
+          "bg-background-secondary absolute top-full z-50",
+          "border border-border-primary rounded-xl overflow-hidden"
+        )}
+      >
+        {options.map((option) => {
+          const isSelected = value === option;
+
+          return (
+            <li
+              key={option}
+              onClick={() => handleSelect(option)}
+              className={clsx(
+                isSelected
+                  ? "bg-background-tertiary"
+                  : "bg-background-secondary hover:bg-background-primary",
+                "cursor-pointer p-8"
+              )}
+            >
+              {option}
+            </li>
+          );
+        })}
+      </ul>
+    );
+
+  return (
+    <div ref={dropdownRef} className="relative inline-block">
+      {renderTrigger()}
+      {renderList()}
     </div>
   );
 }
--- app/test/dropdown/page.tsx
+++ app/test/dropdown/page.tsx
@@ -8,22 +8,33 @@ import Button from "@/app/components/Button/Button";
 
 // Dropdown 컴포넌트는 배열을 받습니다.
 const TEST: string[] = ["1번 선택지", "2번 선택지", "3번 선택지"];
+const TEST2: string[] = ["수정하기", "삭제하기"];
 
 export default function DropdownPage() {
   const [test, setTest] = useState(TEST[0]);
+  const [test2, setTest2] = useState(TEST2[0]);
 
   return (
     <div>
       <Header />
-      <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="flex flex-col justify-center gap-20 items-center min-h-screen">
         <Dropdown
           options={TEST}
           onSelect={setTest}
           size="md"
+          trigger="text"
           value={test}
-          icon="check"
+          icon="toggle"
         />
         <Button label="z-index test" />
+        <Dropdown
+          options={TEST2}
+          onSelect={setTest2}
+          size="md"
+          trigger="icon"
+          value={test2}
+          icon="kebabLarge"
+        />
       </div>
     </div>
   );
