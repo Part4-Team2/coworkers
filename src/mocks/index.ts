c35662cc7ff874ded6c0c4d203c9d775d0e8e67c
k0nghaa <k0nghaalee@gmail.com>
2026-01-04 17:24:39 +0900

    chore: 댓글 목데이터 생성

---
+++ src/mocks/index.ts
@@ -0,0 +1,12 @@
+export const mockComment = [
+  {
+    id: "1",
+    content: "법인 설립 서비스 관련 링크 첨부 드려요 https://www.codeit.kr",
+    userName: "우지은",
+    createdAt: "20분 전", // 시간 포맷팅 2026-01-03T16:26:39.888Z
+  },
+  {
+    id: "2",
+    content: "",
+  },
+];
