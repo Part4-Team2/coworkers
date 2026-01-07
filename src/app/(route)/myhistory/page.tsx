7bc5ade17fd8af5dce2a2bd12365a0b48c2813d2
k0nghaa <k0nghaalee@gmail.com>
2026-01-07 11:05:31 +0900

    chore: 마이 히스토리 페이지 함수명 오타수정

--- src/app/(route)/myhistory/page.tsx
+++ src/app/(route)/myhistory/page.tsx
@@ -1,6 +1,6 @@
 import DoneListContainer from "@/containers/myhistory/DoneListContainer";
 
-export default function MyHisoryPage() {
+export default function MyHistoryPage() {
   return (
     <>
       <h1 className="text-xl font-bold mb-24">마이 히스토리</h1>
