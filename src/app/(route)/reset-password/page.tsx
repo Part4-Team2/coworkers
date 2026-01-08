587611531f3f31df069363025764bb6b2d0fba0f
cadst <tyu1234567@naver.com>
2026-01-08 09:56:55 +0900

    fix: reset -> reset-password 페이지로 변경

--- src/app/(route)/reset-password/page.tsx
+++ src/app/(route)/reset-password/page.tsx
@@ -4,7 +4,7 @@ import ResetContainer from "@/containers/reset/ResetContainer";
 export const metadata = createMetadata({
   title: "비밀번호 재설정",
   description: "비밀번호를 재설정하고 팀원들과 협업을 시작하세요.",
-  url: "/reset",
+  url: "/reset-password",
   alt: "Coworkers - 비밀번호 재설정",
 });
 
