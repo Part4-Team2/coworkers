ff6b70de976cb7afc196915166ff07c83d87080c
cadst <tyu1234567@naver.com>
2026-01-16 16:42:24 +0900

    feat: jose를 이용한 jwt 토큰 디코딩으로 액세스토큰 유효성 판단으로 수정

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getUser } from "@/lib/api/user";
+import { jwtVerify } from "jose";
+
+const secret = new TextEncoder().encode(
+  process.env.JWT_SECRET || "default-secret"
+);
+
+async function isTokenValid(token: string) {
+  try {
+    await jwtVerify(token, secret);
+    return false;
+  } catch {
+    return true;
+  }
+}
 
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
@@ -13,20 +26,29 @@ export async function proxy(request: NextRequest) {
     if (!accessToken) {
       return NextResponse.next();
     }
-    const userData = await getUser();
-    if (!("error" in userData) && "id" in userData && userData.id) {
+    const isValid = await isTokenValid(accessToken);
+    if (isValid) {
       return NextResponse.redirect(new URL("/teamlist", request.url));
     }
+    // const userData = await getUser();
+    // if (!("error" in userData) && "id" in userData && userData.id) {
+    //   return NextResponse.redirect(new URL("/teamlist", request.url));
+    // }
     return NextResponse.next();
   } else {
     if (!accessToken) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
-    const userData = await getUser();
-
-    if ("error" in userData || !("id" in userData) || !userData.id) {
+    const isValid = await isTokenValid(accessToken);
+    if (!isValid) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
+
+    // const userData = await getUser();
+
+    // if ("error" in userData || !("id" in userData) || !userData.id) {
+    //   return NextResponse.redirect(new URL("/login", request.url));
+    // }
 
     return NextResponse.next();
   }
