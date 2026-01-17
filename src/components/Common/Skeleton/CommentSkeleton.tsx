11b3aa4a0c54182471625944135c2ea1e111c1da
kminsoo0110 <kminsoo0110@naver.com>
2026-01-17 16:07:04 +0900

    fix: remove title skeleton

--- src/components/Common/Skeleton/CommentSkeleton.tsx
+++ src/components/Common/Skeleton/CommentSkeleton.tsx
@@ -8,7 +8,7 @@ interface SkeletonProps {
 export default function CommentSkeleton({ commentCount = 3 }: SkeletonProps) {
   return (
     <div className="space-y-40">
-      <div className="h-32 w-150 bg-slate-700 rounded animate-pulse" />
+      {/* <div className="h-32 w-150 bg-slate-700 rounded animate-pulse" /> */}
       <div className="flex flex-col gap-16">
         {Array.from({ length: commentCount }).map((_, index) => (
           <CardSkeleton
